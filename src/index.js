b624351d807d220a767b3915239ecdec259dd2e7
Olga <Mamillaris21@gmail.com>
2023-09-04 11:45:22 +0400

    add requests to api, add react persist

--- src/index.js
+++ src/index.js
@@ -3,12 +3,21 @@ import ReactDOM from 'react-dom/client';
 import { App } from 'components/App';
 import './index.css';
 import { Provider } from 'react-redux';
-import { store } from './Redux/Store';
+import { persistor, store } from 'Redux/Store';
+import { PersistGate } from 'redux-persist/lib/integration/react';
+import { BrowserRouter } from 'react-router-dom';
+import { ChakraProvider } from '@chakra-ui/react';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Provider store={store}>
-      <App />
+      <PersistGate loading={null} persistor={persistor}>
+        <ChakraProvider>
+          <BrowserRouter basename="/goit-react-hw-08-phonebook-Olga">
+            <App />
+          </BrowserRouter>
+        </ChakraProvider>
+      </PersistGate>
     </Provider>
   </React.StrictMode>
 );
