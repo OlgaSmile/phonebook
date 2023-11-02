2ddcfb4c23f5cd72cf103ca6de0585c80d06495c
Smilichenko <112238837+OlgaSmile@users.noreply.github.com>
2023-11-02 10:30:05 +0200

    built

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client';
 import { App } from 'components/App';
 import './index.css';
 import { Provider } from 'react-redux';
-import { persistor, store } from 'Redux/store';
+import { persistor, store } from './Redux/store';
 import { PersistGate } from 'redux-persist/lib/integration/react';
 import { BrowserRouter } from 'react-router-dom';
 import { ChakraProvider } from '@chakra-ui/react';
